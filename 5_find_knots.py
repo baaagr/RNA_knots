1630751823a07023fdd443e6bb46ee35408fc7e1
baaagr <bartosz.gren@student.uw.edu.pl>
2023-02-13 13:07:54 +0100

    ghghgh

--- 5_find_knots.py
+++ 5_find_knots.py
@@ -1,12 +1,16 @@
 import os
 from topoly import alexander
-from topoly.params import TWO_POINTS
+from topoly.params import Closure
+from tqdm import tqdm
 
-def check_alexander()
+def check_alexander():
     xyz_list = os.listdir('xyz')
-    for xyz in xyz_list:
-        res = alexander(xyz, closure=TWO_POINTS, tries=100, max_cross=60, run_parallel=False)
-        print(res)
+    for xyz in tqdm(xyz_list):
+        res = alexander('xyz/{}'.format(xyz), closure=Closure.TWO_POINTS,
+                        tries=100, max_cross=60, run_parallel=False)
+        res = str(res).replace(' ','')[1:-1]
+        with open('alexander.txt', 'w') as f:
+            f.write('{} {}\n'.format(xyz,res))
 
 if __name__ == '__main__':
     print('remember to load venv!')
