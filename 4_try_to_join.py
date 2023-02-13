238dfd247cb898edf906cb7cbc26f7450f98783a
baaagr <bartoszgren@gmail.com>
2023-02-13 12:04:33 +0100

    added 4

--- 4_try_to_join.py
+++ 4_try_to_join.py
@@ -4,4 +4,16 @@ def calc_dist(new_coords, old_coords):
     dist = np.sqrt(np.sum(np.power(new_coords-old_coords,2)))                
     return dist   
 
-wih open
+def get_chains_to_analyze():
+    pdb_chain_dict = {}
+    with open('lists/eq_classes.txt', 'r') as f:
+        for line in f.readlines():
+            eq_class, pdb, chains = line.strip().split()
+            chains = chains.split(',')
+            if len(chains) > 1:
+                pdb_chain_dict[pdb] = chains
+    return pdb_chain_dict
+
+if __name__ == '__main__':
+    print(get_chains_to_analyze())
+        
