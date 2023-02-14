c142bd560dc339a0e8b5782dfb7ec926982918e2
baaagr <bartosz.gren@student.uw.edu.pl>
2023-02-14 13:37:53 +0100

    aaaaa

--- 6_find_knots.py
+++ 6_find_knots.py
@@ -2,11 +2,17 @@ from topoly import alexander
 from topoly.params import Closure
 from tqdm import tqdm
 
-def check_alexander():
+def check_alexander(last_cif):
     xyz_list = []
     with open('lists/xyz_files.txt', 'r') as f:
-        for line in f.readlines():
-            xyz_list.append(line.strip())
+        for i, line in enumerate(f.readlines()):
+            line_s = line.strip()
+            #print('{:3d} {:16} {:16} {}'.format(i+1, last_cif, line_s, str(last_cif == line_s)))
+            if last_cif:
+                if last_cif == line_s:
+                    last_cif = ''
+            else:
+                xyz_list.append(line_s)
     for xyz in tqdm(xyz_list):
     #for xyz in xyz_list:
         res = alexander('xyz/{}'.format(xyz), closure=Closure.TWO_POINTS,
@@ -15,8 +21,17 @@ def check_alexander():
         with open('alexander.txt', 'a+') as f:
             f.write('{} {}\n'.format(xyz,res))
 
+def where_ended():
+    try:
+        with open('alexander.txt', 'r') as f:
+            last_cif = f.readlines()[-1].split()[0]
+    except:
+        last_cif = ''
+    return last_cif
+
 if __name__ == '__main__':
     print('remember to load venv!')
-    with open('alexander.txt', 'w') as f:
-        pass
-    check_alexander()
+    #with open('alexander.txt', 'w') as f:
+    #    pass
+    last_cif = where_ended()
+    check_alexander(last_cif)
